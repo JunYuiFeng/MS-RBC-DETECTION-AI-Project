aeb785e25192898a155b20caba010f3ebfd92d5e
Jason Xie <87727651+JasonXie62@users.noreply.github.com>
2024-05-29 19:36:42 +0200

    added crud

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -4,7 +4,14 @@
     </div>
     <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-violet-900 to-indigo-600">
         <div class="bg-white p-10 w-1/2 rounded-3xl">
-            <table>
+            
+            <h1 class="text-2xl font-black pb-5">Admin Page</h1>
+            <div class="flex justify-end">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded bg-indigo-950 text-sm rounded-xl">
+                    Add User
+                </button>
+            </div>
+            <table class="w-full">
                 <thead>
                     <tr>
                         <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
@@ -12,11 +19,20 @@
                 </thead>
                 <tbody>
                     <tr v-for="(item, index) in items" :key="index">
-                        <td>{{ item.email }}</td>
-                        <td>{{ item.username }}</td>
+                        <td class="p-3">{{ item.email }}</td>
+                        <td class="p-3">{{ item.username }}</td>
+                        <div class="flex justify-end gap-5 pt-3 pb-3">
+                            <button
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded bg-indigo-950 text-sm rounded-xl">Edit User
+                            </button>
+                            <button
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded bg-red-600 text-sm rounded-xl">Delete User
+                            </button>
+                        </div>
                     </tr>
                 </tbody>
-            </table>
+        
+            </table>  
         </div>
     </div>
 </template>
@@ -24,7 +40,7 @@
 <script setup lang="ts">
 import NavBar from '/src/components/NavBar.vue'
 
-const headers = ['Email', 'Username', ]
+const headers = ['Email', 'Username']
 const items = [
     { email: 'gmail.com', username: 'Jason' },
     { email: 'yahoo.com', username: 'Jun' },
@@ -35,4 +51,8 @@ const items = [
 
 </script>
 
-<style scoped></style>
+<style scoped> 
+tr {
+    border-bottom: 1px solid #ddd;
+}
+</style>
