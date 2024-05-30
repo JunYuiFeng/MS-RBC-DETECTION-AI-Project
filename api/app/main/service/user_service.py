8d206a66b8e38cb19a339d584af927c6ce6e842b
Y. Roes <690101@student.inholland.nl>
2024-05-30 02:13:51 +0200

    feature/user-crud: added service layer for user ops

---
+++ api/app/main/service/user_service.py
@@ -0,0 +1,32 @@
+from db import query_db
+from flask_restx import Resource
+
+class user_service(Resource):
+  
+  @staticmethod
+  def get_all():
+    users = query_db("SELECT username, email, passwd FROM users")
+    return users;
+  
+  # can return boolean since query only returns 1 or 0
+  @staticmethod
+  def get_by_email_and_passwd(email: str, passwd: str):
+    user_exists = query_db("SELECT EXISTS (SELECT 1 FROM users WHERE email = ? AND passwd = ?)", [email, passwd])
+    return bool(user_exists)
+  
+  @staticmethod
+  def get_by_id(id: int):
+    data = query_db("SELECT id, username, email FROM users WHERE id = ?", [id], one=True)
+    return {
+      "id": data[0],
+      "username": data[1],
+      "email": data[2]
+    }
+     
+  @staticmethod
+  def create_user(username: str, email: str, passwd: str):
+    return query_db("INSERT INTO users (username, email, passwd) VALUES (?, ?, ?)", [username, email, passwd])
+    
+  @staticmethod
+  def delete_by_id(id: int):
+    return query_db("DELETE FROM users WHERE id = ?", [id])
