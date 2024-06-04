afcb182451435a0b0d46ac13bdeca6a7a1317a20
Y. Roes <690101@student.inholland.nl>
2024-06-04 17:52:35 +0200

    feature/user-crud: removed api.model from status_dto

--- api/app/main/util/dto.py
+++ api/app/main/util/dto.py
@@ -3,11 +3,6 @@ from flask_restx import Namespace, fields
 
 class status_dto:
     api = Namespace('status', description='status operations')
-    user = api.model('status', {
-        'email': fields.String(required=True, description='user email address'),
-        'username': fields.String(required=True, description='user username'),
-        'password': fields.String(required=True, description='user password'),
-    })
     
     
 class auth_dto:
