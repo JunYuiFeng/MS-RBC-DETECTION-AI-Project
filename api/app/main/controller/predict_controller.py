d709d2c6bfc4b94394c4face284ed79351e8eaad
Sahib <sahibzulfigar4@gmail.com>
2024-06-04 17:37:26 +0200

    predict endpoint finished

--- api/app/main/controller/predict_controller.py
+++ api/app/main/controller/predict_controller.py
@@ -1,6 +1,9 @@
 # app/controller/predict_controller.py
+import base64
+import cv2
 from flask_restx import Namespace, Resource
 from flask import request, jsonify, send_file
+import numpy as np
 import torch
 from torchvision import transforms
 from app.main.model.yolov8_model import load_model
@@ -11,18 +14,9 @@ predict_ns = Namespace('predict', description='Prediction operations')
 
 model = load_model()
 
-def recursive_tensor_to_list(value):
-    """ Recursively convert tensors in nested lists and tuples to lists. """
-    if isinstance(value, torch.Tensor):
-        return value.tolist()  # Convert tensors to list
-    elif isinstance(value, (list, tuple)):
-        return [recursive_tensor_to_list(item) for item in value]  # Recurse into lists/tuples
-    return value  # Return non-tensor items unchanged
-
 @predict_ns.route('/')
 class Predict(Resource):
     def post(self):
-        print(">>>> PREDICT")
         if 'image' not in request.files:
             return {'error': 'No image uploaded'}, 400
 
@@ -30,59 +24,27 @@ class Predict(Resource):
         image = Image.open(io.BytesIO(file.read()))
 
         transform = transforms.Compose([
-        transforms.Resize((640, 640)),  # Adjust size according to your model's requirements
+        transforms.Resize((640, 640)),  
         transforms.ToTensor(),  # Converts the image to a Tensor
-        # Add normalization if your model was trained with it:
-        # transforms.Normalize(mean=[mean values], std=[std values]),
         ])
     
         # Apply the transformations
         image = transform(image).unsqueeze(0)  # Add batch dimension
-        
         image = image.float()
 
-        # Perform inference
-        with torch.no_grad():
-            outputs = model(image)
-    
-        
-        # Format the results as needed
-        # predictions = outputs.tolist()
-        print(">>>> Results")
-         # Check output type and structure
-        # print(outputs)
-        for tensor in outputs:
-            if isinstance(tensor, torch.Tensor):
-                print("Tensor shape:", tensor.shape)
-            elif isinstance(tensor, list):
-            # If the element is a list of tensors, print each tensor's shape
-                for t in tensor:
-                    print("Tensor in list shape:", t.shape)
-            else:
-                print("Element is neither tensor nor list")
-    
-        predictions = recursive_tensor_to_list(outputs)
+        results = model(image, conf=0.6)
 
-        return jsonify({'predictions': predictions})
+        for result in results:
+            boxes = result.boxes  # Boxes object for bounding box outputs
+            unique, counts = np.unique(boxes.cls.numpy(), return_counts=True)
+            annotated_image = result.plot()  # Get the annotated image as NumPy array
 
-        # Annotate image
-        annotated_img = results.render()[0]
-        annotated_img_pil = Image.fromarray(annotated_img)
-
-        # Convert image to byte array
-        img_io = io.BytesIO()
-        annotated_img_pil.save(img_io, 'JPEG')
-        img_io.seek(0)
-
-        # Extract other results if needed
-        detections = results.pandas().xyxy[0].to_dict(orient="records")
-
+            # Convert image to bytes (assuming RGB format)
+            encoded_image = base64.b64encode(annotated_image.tobytes()).decode('utf-8')
+       
         return jsonify({
-            'annotations': detections,
-            'image': request.url_root + 'predict/annotated_image'
-        })
+            'deformedCellsDetected': int(counts[0]),
+            'healthyCellsDetected': int(counts[1]),
+            'annotatedImage': encoded_image
+                        })
 
-@predict_ns.route('/annotated_image')
-class AnnotatedImage(Resource):
-    def get(self):
-        return send_file(img_io, mimetype='image/jpeg')
--- api/app/main/model/yolov8_model.py
+++ api/app/main/model/yolov8_model.py
@@ -1,11 +1,7 @@
-import torch
 import os
-# from model import CustomYOLOv8
+from ultralytics import YOLO
 
 def load_model() :
     model_path = os.path.join(os.path.dirname(__file__), 'yolov8m_v2.0.pt')
-    model_dict = torch.load(model_path, map_location=torch.device('cpu'))
-    model = model_dict['model']  # Extract the model
-    model = model.float()  # Ensure the model is in full precision
-    model.eval()  # Set the model to evaluation mode
-    return model
+    model = YOLO(model_path)
+    return model 
